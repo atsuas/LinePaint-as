bae756372ddebfafeb3cc20a1820488a837c7781
as <>
2021-09-16 00:34:05 +0900

    GridScript,LevelManager作成、ブロック配列

---
+++ Assets/Script/GridScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LinePaint
+{
+
+    public class GridScript
+    {
+        private int _width;
+        private int _height;
+        private float _cellSize;
+
+        private int[,] gridArray;
+
+        public int[,] GridArray { get { return gridArray; } }
+
+        public void Initialize(int width, int height, float cellSize)
+        {
+            _width = width;
+            _height = height;
+            _cellSize = cellSize;
+
+            gridArray = new int[width, height];
+        }
+
+    }
+
+}
---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LinePaint
+{
+
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject blockPrefab;
+        [SerializeField] private int width;
+        [SerializeField] private int height;
+        [SerializeField] private float cellSize;
+
+        private GridScript gridScript;
+
+        private void Start()
+        {
+            gridScript = new GridScript();
+            gridScript.Initialize(width, height, cellSize);
+
+            CreateGrid();
+        }
+
+        private void CreateGrid()
+        {
+            for (var x = 0; x < gridScript.gridArray.GetLength(0); x++)
+            {
+                for (var x = 0; x < gridScript.gridArray.GetLength(1); x++)
+                {
+                    GameObject block = Instantiate(blockPrefab);
+                }
+            }
+        }
+
+    }
+
+}
