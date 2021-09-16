b8edf22afad7043b2632b4760307a37999148970
as <>
2021-09-16 21:30:50 +0900

    カメラ位置設定、cell作成

---
+++ Assets/Script/CameraZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LinePaint
+{
+
+    public class CameraZoom : MonoBehaviour
+    {
+        private Camera myCam;
+
+        private void Awake()
+        {
+            myCam = GetComponent<Camera>();
+        }
+
+        public void ZoomPerspectiveCamera(float width, float height)
+        {
+            float _height = 2.0f * ((width > height ? width : height + 0.5f) / 2) * Mathf.Atan(myCam.fieldOfView);
+
+            if (_height < 5.5f)
+            {
+                _height = 5.5f;
+            }
+
+            transform.position = new Vector3((width - 1) / 2f, _height, myCam.transform.position.z);
+        }
+    }
+}
--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -7,11 +7,13 @@ namespace LinePaint
 
     public class LevelManager : MonoBehaviour
     {
-        [SerializeField] private GameObject blockPrefab;
+        [SerializeField] private CameraZoom gameCamera;
+        [SerializeField] private Cell blockPrefab;
         [SerializeField] private int width;
         [SerializeField] private int height;
         [SerializeField] private float cellSize;
 
+        private Cell[,] cells;
         private GridScript gridScript;
         private SwipeController swipeController;
 
@@ -22,20 +24,32 @@ namespace LinePaint
 
             gridScript = new GridScript();
             gridScript.Initialize(width, height, cellSize);
+            cells = new Cell[width, height];
 
-            CreateGrid();
+            CreateGrid(Vector3.zero);
+            //追加
+            gameCamera.ZoomPerspectiveCamera(width, height);
+
         }
 
-        private void CreateGrid()
+        private void CreateGrid(Vector3 originPos)
         {
             for (int x = 0; x < gridScript.GridArray.GetLength(0); x++)
             {
                 for (int y = 0; y < gridScript.GridArray.GetLength(1); y++)
                 {
-                    GameObject block = Instantiate(blockPrefab);
-                    block.transform.position = gridScript.GetCellWorldPosition(x, y);
+                    cells[x, y] = CreateCell(x, y, originPos);
                 }
             }
+        }
+
+        private Cell CreateCell(int x, int y, Vector3 originPos)
+        {
+            Cell cell = Instantiate(blockPrefab);
+            cell.coords = new Vector2Int(x, y);
+            cell.transform.localScale = new Vector3(cellSize, 0.25f, cellSize);
+            cell.transform.position = originPos + gridScript.GetCellWorldPosition(x, y);
+            return cell;
         }
 
         public void MoveBrush(Swipe direction)
